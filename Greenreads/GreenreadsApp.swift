9db7d39d21a1d9a77f52b64789099fb889b5f7f6
multimokia <multimokia3@gmail.com>
2023-02-05 20:56:25 -0500

    Initial commit

---
+++ Greenreads/GreenreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenreadsApp.swift
+//  Greenreads
+//
+//  Created by Misty D'mello on 2023-02-05.
+//
+
+import SwiftUI
+
+@main
+struct GreenreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
