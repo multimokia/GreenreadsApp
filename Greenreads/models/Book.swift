af2bdf9ab852034853094aa4333be778722e431d
multimokia <multimokia3@gmail.com>
2023-03-21 22:19:09 -0400

    Add the lib and models folders back

--- Greenreads/models/Book.swift
+++ Greenreads/models/Book.swift
@@ -8,5 +8,15 @@
 import Foundation
 
 struct Book: Encodable, Decodable {
-
+    let id: Int?;
+    let title: String;
+    let author: String;
+    let ISBN: String?;
+    let publication_date: Date;
+    let cover_image: String?;
+    let summary: String;
+    let rating: Float;
+    let created_at: Date?;
+    let updated_at: Date?;
+    let deleted_at: Date?;
 }
